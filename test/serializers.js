d8b10baf1fa3d0eb4a162f7b0152920d071eca6c
bergi <bergi@axolotlfarm.org>
2015-08-19 00:00:02 +0200

    added SparqlUpdateSerializer

--- test/serializers.js
+++ test/serializers.js
@@ -8,6 +8,51 @@
 })(this, function () {
   return function (assert, rdf, readFile, utils, ctx) {
     describe('serializers', function () {
+      it('N-Triples serializer should generate parsable card', function (done) {
+        var
+          serializer = new rdf.promise.Serializer(new rdf.NTriplesSerializer()),
+          parser = new rdf.promise.Parser(new rdf.NTriplesParser());
+
+        serializer.serialize(ctx.cardGraph)
+          .then(function (card) {
+            return parser.parse(card, 'https://www.example.com/john/card');
+          })
+          .then(function (graph) {
+            return utils.p.assertGraphEqual(graph, ctx.cardGraph);
+          })
+          .then(function () {
+            done()
+          })
+          .catch(function (error) {
+            done(error);
+          });
+      });
+
+      it('SPARQL Update serializer should generate parsable card', function (done) {
+        var
+          serializer = new rdf.promise.Serializer(new rdf.SparqlUpdateSerializer()),
+          parser = new rdf.promise.Parser(new rdf.NTriplesParser());
+
+        serializer.serialize(ctx.cardGraph)
+          .then(function (card) {
+            assert.equal(card.substring(0, 13), 'INSERT DATA {');
+            assert.equal(card.substring(card.length-1), '}');
+
+            card = card.substring(13, card.length-1).trim();
+
+            return parser.parse(card, 'https://www.example.com/john/card');
+          })
+          .then(function (graph) {
+            return utils.p.assertGraphEqual(graph, ctx.cardGraph);
+          })
+          .then(function () {
+            done()
+          })
+          .catch(function (error) {
+            done(error);
+          });
+      });
+
       it('Turtle serializer should generate parsable card', function (done) {
         var
           serializer = new rdf.promise.Serializer(new rdf.TurtleSerializer()),
