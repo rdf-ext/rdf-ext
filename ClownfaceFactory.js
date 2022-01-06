040997413cea434a39712258e40fecc667061c68
bergi <bergi@axolotlfarm.org>
2022-01-06 14:51:35 +0100

    feat: create Dataset in ClownfaceFactory if none is given

--- ClownfaceFactory.js
+++ ClownfaceFactory.js
@@ -2,6 +2,10 @@ import clownface from 'clownface'
 
 class ClownfaceFactory {
   clownface ({ ...args } = {}) {
+    if (!args.dataset && typeof this.dataset === 'function') {
+      args.dataset = this.dataset()
+    }
+
     return clownface({ ...args, factory: this })
   }
 }
